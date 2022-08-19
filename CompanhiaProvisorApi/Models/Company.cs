6826f811f6c79c03ac554aca942083c9355bf833
wallaceleonel <wallace.leonell@outlook.com>
2022-08-19 19:19:15 -0300

    iniciando projeto, criando classes de contexto e dockerfile

---
+++ CompanhiaProvisorApi/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanhiaProvisorApi.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        [Required]
+        public string? FantasyName { get; set; }
+        [Required, MaxLength(2)]
+        public string? Uf { get; set; }
+        [Required, MaxLength(14)]
+        public int Cnpj { get; set; }
+        public virtual Provider Provider { get; set; }
+    }
+}
---
+++ CompanhiaProvisorApi/Models/Provider.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanhiaProvisorApi.Models
+{
+
+    [Table(name:"Provider")]
+    public class Provider
+    {
+        public Provider()
+        {
+            this.Companys = new HashSet<Company>();
+        }
+        [Key]
+        public int ProviderId { get; set; }
+        [Required]
+        public string? FantasyName { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public int Document { get; set; }
+
+        public DateTime Date { get; set; }
+        public virtual ICollection<Company> Companys { get; set; }  
+    }
+}
