f5c95b3b4c25fd78af44bc68f0762a813b1dedc1
wallaceleonel <wallace.leonell@outlook.com>
2022-08-22 20:58:04 -0300

    adicionando front em react

---
+++ react_playmove/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+//import api from '.api';
+import { Component } from 'react';
+
+class App extends Component{
+
+  render(){
+    return(
+      <div>
+        <h1>Listar fornecedores teste</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
