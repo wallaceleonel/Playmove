85069313d70781d236ae11a958f39155516fc289
wallaceleonel <wallace.leonell@outlook.com>
2022-08-22 16:48:23 -0300

    add controllers

--- CompanhiaProvisorApi/Controllers/ProvidersController.cs
+++ CompanhiaProvisorApi/Controllers/ProvidersController.cs
@@ -1,9 +1,12 @@
-﻿using CompanhiaProvisorApi.Data;
-using CompanhiaProvisorApi.DTO;
-using CompanhiaProvisorApi.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using CompanhiaProvisorApi.Data;
+using CompanhiaProvisorApi.Models;
 
 namespace CompanhiaProvisorApi.Controllers
 {
@@ -17,37 +20,103 @@ namespace CompanhiaProvisorApi.Controllers
         {
             _context = context;
         }
+
+        // GET: api/Providers
         [HttpGet]
-        public async Task<ActionResult<List<Provider>>> Get(int CompanyId)
+        public async Task<ActionResult<IEnumerable<Provider>>> GetProviders()
         {
-            var providers = await _context.Providers
-                .Where(x => x.CompanyId == CompanyId)
-                .ToListAsync();
-            return providers;
+          if (_context.Providers == null)
+          {
+              return NotFound();
+          }
+            return await _context.Providers.ToListAsync();
         }
 
-        [HttpPost]
-        public async Task<ActionResult<List<Provider>>> Create(CreateProviderDTO request)
+        // GET: api/Providers/
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Provider>> GetProvider(int id)
         {
-            var provider = await _context.Providers.FindAsync(request.CompanyId);
+          if (_context.Providers == null)
+          {
+              return NotFound();
+          }
+            var provider = await _context.Providers.FindAsync(id);
+
             if (provider == null)
-                return NotFound();
-
-            var newProvider = new Provider
             {
-                Name = request.Name,
-                Document = request.Document,
-                Phone = request.Phone,
-                DateCredential = request.DateCredential,
-                
-                Company = provider.Company
-            };
+                return NotFound();
+            }
 
+            return provider;
+        }
 
-            _context.Providers.Add(newProvider);
+        // PUT: api/Providers/
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProvider(int id, Provider provider)
+        {
+            if (id != provider.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(provider).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProviderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Providers
+        [HttpPost]
+        public async Task<ActionResult<Provider>> PostProvider(Provider provider)
+        {
+          if (_context.Providers == null)
+          {
+              return Problem("Entity set 'DataContext.Providers'  is null.");
+          }
+            _context.Providers.Add(provider);
             await _context.SaveChangesAsync();
 
-            return await Get(newProvider.CompanyId);
+            return CreatedAtAction("GetProvider", new { id = provider.Id }, provider);
+        }
+
+        // DELETE: api/Providers/
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProvider(int id)
+        {
+            if (_context.Providers == null)
+            {
+                return NotFound();
+            }
+            var provider = await _context.Providers.FindAsync(id);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+
+            _context.Providers.Remove(provider);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProviderExists(int id)
+        {
+            return (_context.Providers?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
--- CompanhiaProvisorApi/DTO/ProviderDTO.cs
+++ CompanhiaProvisorApi/DTO/ProviderDTO.cs
@@ -1,9 +1,9 @@
 ﻿namespace CompanhiaProvisorApi.DTO
 {
-    public class CreateProviderDTO
+    public class ProviderDTO
     {
         public int CompanyId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         public int Document { get; set; }
         public int Phone { get; set; }
