a0c58fbcd2150f5e2e0361e7142cc9d1a4d06791
wallaceleonel <wallace.leonell@outlook.com>
2022-08-22 21:35:38 -0300

    correção do erro

--- react_playmove/src/App.js
+++ react_playmove/src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
-//import api from '.api';
+import api from '.api';
 import { Component } from 'react';
 
 class App extends Component{
 
-  render(){
+  state= {
+
+    Fornecedores: [],
+  }
+  async componentDidMount()
+  {
+    const response = await api.get('');
+
+    this.setState({Fornecedores : response.data});
+  }
+
+  render()
+  {
+    const {Fornecedores} = this.state;
     return(
       <div>
-        <h1>Listar fornecedores teste</h1>
+        <h1> fornecedores </h1>
+        {Fornecedores.map(Fornecedores =>(
+          <li key={Fornecedores.show.id}>
+            <h2>
+              <strong> Fornecedor : </strong>
+              {Fornecedores.show.name}
+            </h2>
+          </li>
+        ))}
       </div>
     )
   }
