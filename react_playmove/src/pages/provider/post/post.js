4dc1c2ec4bfb4f7a115e30448e5f2e410b5f42d1
wallaceleonel <wallace.leonell@outlook.com>
2022-08-25 22:21:33 -0300

    postProvider

--- react_playmove/src/pages/provider/post/post.js
+++ react_playmove/src/pages/provider/post/post.js
@@ -1,11 +1,63 @@
 import React from "react";
 
-import Header from "../../../components/Header/header";
+import Header from '../../../components/HeaderProvider/headerProvider'
 
 function PostProvider(){
     return(
         <div>
-            <Header/>
+            <HeaderProvider/>
+                         <main>
+
+                <div className="card-post" >
+
+                    <h1>Adicionar Companhia</h1>
+                    <div className="line-post" ></div>
+
+                    <div className="card-body-post" >
+
+                        <form onSubmit={handleSubmit(addPost)} >
+
+                            <div className="fields" >
+                                <label>Fornecedor</label>
+                                <input type="text" name="fantastName" {...register("fantasyName")} />
+                                <p className="error-message">{errors.fantasyName?.message}</p>
+                            </div>
+
+                            <div className="fields" >
+                                <label>Documento</label>
+                                <input type="number" name="document" {...register("document")} />
+                                <p className="error-message">{errors.document?.message}</p>
+                            </div>
+                            <div className="fields" >
+                                <label>Telefone</label>
+                                <input type="tel" name="phone" {...register("phone")} />
+                                <p className="error-message">{errors.phone?.message}</p>
+                            </div>
+                            <div className="fields" >
+                                <label>Data</label>
+                                <input type="datetime" name="data" {...register("data")} />
+                                <p className="error-message">{errors.data?.message}</p>
+                            </div>
+
+                            <div className="fields">
+                                <label>Companhia</label>
+                                <select {...register("fantasyName", { required: true })}>
+                                    <option value="fanstasyName">{fantasyName}</option>
+                                </select>
+                                <p className="error-message">{errors.fantasyName?.message}</p>
+                            </div>
+
+                            <div className="btn-post" >
+                                <button type="submit" > Cadastrar fornecedor </button>
+                            </div>
+
+                        </form>
+
+                    </div>
+
+                </div>
+
+            </main>
         </div>
     )
 }
